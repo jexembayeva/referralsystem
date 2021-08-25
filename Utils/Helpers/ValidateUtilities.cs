6e6cc80d4da44daabaa3e54d9cbc6d63cf4e1dbc
jexembayeva gaukhar <jexembayevag@gmail.com>
2021-08-25 14:24:12 +0600

    fix

--- Utils/Helpers/ValidateUtilities.cs
+++ Utils/Helpers/ValidateUtilities.cs
@@ -25,57 +25,10 @@ namespace Utils.Helpers
             throw exception;
         }
 
-        public static void ThrowIfNullOrEmpty<T>(this T[] collection, string paramName)
-            where T : class
-        {
-            paramName.ThrowIfNullOrEmpty(nameof(paramName));
-            collection.ThrowIfNull(paramName);
-
-            if (!collection.Any())
-            {
-                throw new InvalidOperationException($"You should not pass empty collection: {paramName}");
-            }
-        }
-
-        public static void ThrowIfNullOrEmpty<T>(this IReadOnlyCollection<T> collection, string paramName)
-            where T : class
-        {
-            paramName.ThrowIfNullOrEmpty(nameof(paramName));
-            collection.ThrowIfNull(paramName);
-
-            if (!collection.Any())
-            {
-                throw new InvalidOperationException($"You should not pass empty collection: {paramName}");
-            }
-        }
-
-        public static void ThrowIfNullOrEmpty<T>(this ICollection<T> collection, string paramName)
-            where T : class
-        {
-            (collection?.ToArray()).ThrowIfNullOrEmpty(paramName);
-        }
-
         public static bool NullOrEmpty(this string @string)
         {
             return string.IsNullOrEmpty(@string?.Trim());
         }
-
-        public static void ThrowIfNullOrEmpty(this string @string, string paramName)
-        {
-            if (paramName.NullOrEmpty())
-            {
-                throw new InvalidOperationException("You should not pass null or empty string a paramName");
-            }
-
-            if (@string.NullOrEmpty())
-            {
-                throw new ArgumentNullException(paramName: paramName);
-            }
-        }
-
-        public static bool IsDefaultValue(this double @double)
-        {
-            return Math.Abs(@double - default(double)) < 0.01;
-        }
+        
     }
 }
