fb1b41ee050d34abded56fab6763422e3a331a73
jexembayeva gaukhar <jexembayevag@gmail.com>
2021-08-26 15:59:54 +0600

    refactor

--- ReferralSystem.Api/Controllers/CrudControllerBase.cs
+++ ReferralSystem.Api/Controllers/CrudControllerBase.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using ReferralSystem.Database.Repositories.Base;
 using Utils.Interfaces;
 
-namespace ReferralSystem.Controllers
+namespace ReferralSystem.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
--- ReferralSystem.Api/Program.cs
+++ ReferralSystem.Api/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace ReferralSystem
+namespace ReferralSystem.Api
 {
     public class Program
     {
